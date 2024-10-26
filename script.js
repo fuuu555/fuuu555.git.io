ed2542725c39c10f9014d6790458e75741a62df2
fuuu555 <joeph20050505@gmail.com>
2024-10-26 20:04:55 +0800

    new

--- script.js
+++ script.js
@@ -23,5 +23,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // 觀察技能區域
         observer.observe(skillsSection);
-    });
+        
+});
 
+document.addEventListener("DOMContentLoaded", () => {
+    const imageGroups = [
+        ["img/emg1.jpg", "img/emg1-2.jpg", "img/emg1-3.jpg"], // Group 1
+        ["img/emg2.jpg", "img/emg2-2.jpg", "img/emg2-3.jpg"], // Group 2
+        ["img/emg3.jpg", "img/emg3-2.jpg", "img/emg3-3.jpg"]  // Group 3
+    ];
+    
+    let currentIndices = [0, 0, 0]; // 每組的當前圖片索引
+    
+        // 切換圖片的函數
+    window.changeImage = function(direction, groupIndex) {
+        currentIndices[groupIndex] = (currentIndices[groupIndex] + direction + imageGroups[groupIndex].length) % imageGroups[groupIndex].length;
+    
+            // 獲取當前組的圖片容器
+        const imageContainer = document.querySelectorAll(".eimg")[groupIndex];
+    
+            // 更新容器中的第一張圖片的 src
+        const imageElement = imageContainer.querySelector("img");
+        imageElement.src = imageGroups[groupIndex][currentIndices[groupIndex]];
+    };
+});
+    
